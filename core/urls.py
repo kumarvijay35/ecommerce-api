9d122075f2381b2c000d8371c3b2d79235c4e186
kumarvijay35 <nameisvijau35@gmail.com>
2026-09-05 22:07:18 +0530

    Add API root view

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,6 @@ URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.http import JsonResponse
@@ -24,21 +14,6 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     SpectacularRedocView
 )
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', api_root),
-    path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
-    path('api/products/', include('products.urls')),
-    path('api/orders/', include('orders.urls')),
-
-    # Swagger
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 def api_root(request):
@@ -56,3 +31,16 @@ def api_root(request):
     })
 
 
+urlpatterns = [
+    path('', api_root),
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/orders/', include('orders.urls')),
+
+    # OpenAPI schema and docs
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
